843bb511cc195a9268b3a030cc7bae5f3a1ea27f
segmentio <segmentio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 23:43:03 +0000

    Tagging version 1.0.9
    git-svn-id: https://plugins.svn.wordpress.org/segmentio/tags/1.0.9@1067492 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- analytics-wordpress.php
+++ analytics-wordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: Analytics for WordPress — by Segment.io
 Plugin URI: https://segment.io/plugins/wordpress
 Description: The hassle-free way to integrate any analytics service into your WordPress site.
-Version: 1.0.4
+Version: 1.0.9
 License: GPLv2
 Author: Segment.io
 Author URI: https://segment.io
@@ -181,7 +181,7 @@ class Segment_Analytics_WordPress {
 	/**
 	 * Current plugin version.
 	 */
-	const VERSION = '1.0.5';
+	const VERSION = '1.0.8';
 
 	/**
 	 * The singleton instance of Segment_Analytics_WordPress.
@@ -820,7 +820,7 @@ class Segment_Analytics_WordPress {
 			if ( is_single() && ! is_attachment() ) {
 
 				if ( ! self::is_excluded_post_type() ) {
-					$categories = implode( ', ', wp_list_pluck( get_categories( get_the_ID() ), 'name' ) );
+					$categories = implode( ', ', wp_list_pluck( get_the_category( get_the_ID() ), 'name' ) );
 					$track = array(
 						'event'      => sprintf( __( 'Viewed %s', 'segment' ), ucfirst( get_post_type() ) ),
 						'properties' => array(
